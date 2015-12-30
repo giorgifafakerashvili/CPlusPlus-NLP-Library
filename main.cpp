8db0310a820221110d0f3b32ac836a275344140f
giorgi <giorgi.fafa@gmail.com>
2015-12-30 14:53:18 +0400

    Modified utils.cpp added split method and tokenize class

--- main.cpp
+++ main.cpp
@@ -14,66 +14,7 @@
 #include "freqdist.cpp"
 
 
-std::vector<std::string> readFile(const std::string& fileName) {
-  std::vector<std::string> vec;
 
-  // Here must be implementation of reading words from file line by line 
-  // There must be one word on each liine
-
-
-  return vec;
-}
-
-
-
-std::vector<std::string> split(const std::string& text, char tokenizer = ' ') {
-  std::vector<std::string> vec;
-
-
-  std::string str = "";
-  int i = 0;
-  for(;i < text.size(); i++) {
-    if(text[i] == tokenizer) {
-      if(str != " " && str.size() > 0) 
-        vec.push_back(str);
-      str = "";
-    } else {
-      str+=text[i];
-    }
-  }
-
-  if(str != " " && str.size() > 0)
-    vec.push_back(str);
-
-  return vec;
-}
-  
-
-class Tokenize  {
-private:
-  std::vector<std::string> words;
-
-public:
-  Tokenize() {}
-  ~Tokenize() {}
-
-  std::vector<std::string> WordTokenize(const std::string& text) {
-    return split(text, ' ');
-  }
-
-  std::vector<std::string> LineTokenize(const std::string& text) {
-    return split(text, '\n');
-  }
-
-  std::vector<std::string> TabTokenize(const std::string& text) {
-    return split(text, '\t');
-  }
-
-  std::vector<std::string> CharTokenize(const std::string& text, char delimiter = ' ') {
-    return split(text, delimiter);
-  }
-
-};
 
 
 
--- utils.cpp
+++ utils.cpp
@@ -11,3 +11,53 @@ std::ostream& operator<<(std::ostream& out, const std::vector<std::string>& word
 
 	out << "]";
 }
+
+std::vector<std::string> split(const std::string& text, char tokenizer = ' ') {
+  std::vector<std::string> vec;
+
+
+  std::string str = "";
+  int i = 0;
+  for(;i < text.size(); i++) {
+    if(text[i] == tokenizer) {
+      if(str != " " && str.size() > 0) 
+        vec.push_back(str);
+      str = "";
+    } else {
+      str+=text[i];
+    }
+  }
+
+  if(str != " " && str.size() > 0)
+    vec.push_back(str);
+
+  return vec;
+}
+
+class Tokenize  {
+private:
+  std::vector<std::string> words;
+
+public:
+  Tokenize() {}
+  ~Tokenize() {}
+
+  std::vector<std::string> WordTokenize(const std::string& text) {
+    return split(text, ' ');
+  }
+
+  std::vector<std::string> LineTokenize(const std::string& text) {
+    return split(text, '\n');
+  }
+
+  std::vector<std::string> TabTokenize(const std::string& text) {
+    return split(text, '\t');
+  }
+
+  std::vector<std::string> CharTokenize(const std::string& text, char delimiter = ' ') {
+    return split(text, delimiter);
+  }
+
+};
+
+
